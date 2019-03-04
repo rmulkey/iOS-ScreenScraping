cb12a7de1d4f70d0624bf77d786217c15909b6c7
rodrigo.s.mulkey <rodrigo.s.mulkey@accenture.com>
2019-03-04 17:46:50 -0500

    setting up UI

--- SpikeScreenScraping/SpikeScreenScraping/LoginViewController.swift
+++ SpikeScreenScraping/SpikeScreenScraping/LoginViewController.swift
@@ -8,14 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        //Test
-        
+                
     }
 
 
